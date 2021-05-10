1853c1461719a072cae1ad33c746e0f75c09dd61
gucci8 <kusti.hamalainen@aalto.fi>
2021-05-10 22:57:01 +0300

    unicafe submission

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const Button = ({ clickHandler, text }) => (
+  <button onClick={clickHandler}>
+    {text}
+  </button>
+)
+
+const Statistics = (props) => {
+  if (props.all === 0) {
+    return (
+      <div>
+        No feedback given
+      </div>
+    )
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <table>
+      <tbody>
+      <StatisticLine text={'Good'} value={props.good} />
+      <StatisticLine text={'Neutral'} value={props.neutral} />
+      <StatisticLine text={'Bad'} value={props.bad} />
+      <StatisticLine text={'All'} value={props.all} />
+      <StatisticLine text={'Average'} value={props.avg} />
+      <StatisticLine text={'Positive'} value={props.pos} unit={'%'} />
+      </tbody>
+    </table>
+  )
 }
 
-export default App;
+const StatisticLine = (props) => (
+  <tr>
+    <td>{props.text}</td>
+    <td>{props.value}{props.unit}</td>
+  </tr>
+)
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const all = good+neutral+bad
+  const avg = (good-bad)/all
+  const pos = 100*good/all
+
+  const goodHandler = () => setGood(good + 1)
+  const neutralHandler = () => setNeutral(neutral + 1)
+  const badHandler = () => setBad(bad + 1)
+
+  return (
+    <div>
+      <h1>Give feedback:</h1>
+      <div>
+        <Button clickHandler={goodHandler} text={'Good'} />
+        <Button clickHandler={neutralHandler} text={'Neutral'} />
+        <Button clickHandler={badHandler} text={'Bad'} />
+      </div>
+      <h1>Statistics:</h1>
+        <Statistics good={good} neutral={neutral} bad={bad} all={all} avg={avg} pos={pos} />
+    </div>
+  )
+}
+
+export default App
