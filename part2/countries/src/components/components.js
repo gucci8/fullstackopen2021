2d0038eb6170f14476f1e9c9f76ea0c838bfd24d
gucci8 <kusti.hamalainen@aalto.fi>
2021-05-17 10:24:18 +0300

    Little fixes

--- part2/countries/src/components/components.js
+++ part2/countries/src/components/components.js
@@ -75,7 +75,7 @@ const Country = (props) => (
 
 const getWeather = async(capital) => {
   const api_key = process.env.REACT_APP_API_KEY
-  const response = await axios.get(`api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${api_key}`)
+  const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${api_key}`)
   return response.data
 }
 
