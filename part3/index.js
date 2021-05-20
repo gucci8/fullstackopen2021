ec9d9919359f9a8ffbf0d3e1910e172f3466a673
gucci8 <kusti.hamalainen@aalto.fi>
2021-05-20 13:20:36 +0300

    3.7-3.8 submission

--- part3/index.js
+++ part3/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
+var morgan = require("morgan");
+
+morgan.token('body', (req) => JSON.stringify(req.body))
 
 app.use(express.json());
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 let persons = [
   {
@@ -24,33 +28,28 @@ let persons = [
     number: "39-23-6423122",
     id: 4,
   },
-  {
-    name: "Meri Kilke",
-    number: "39-23-6423122",
-    id: 5,
-  },
 ];
 
 const generateId = () => {
-  let id = Math.floor(Math.random() * 10000)
-  while (persons.map(p => p.id).includes(id)) {
-    id = Math.floor(Math.random() * 10000)
+  let id = Math.floor(Math.random() * 10000);
+  while (persons.map((p) => p.id).includes(id)) {
+    id = Math.floor(Math.random() * 10000);
   }
-  return id
-}
+  return id;
+};
 
 app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(p => p.id === id)
+  const id = Number(request.params.id);
+  const person = persons.find((p) => p.id === id);
 
   if (person) {
-      response.json(person)
+    response.json(person);
   } else {
-      response.status(404).end()
+    response.status(404).end();
   }
 });
 
@@ -62,44 +61,44 @@ app.get("/info", (request, response) => {
   );
 });
 
-app.post('/api/persons', (request, response) => {
-  const body = request.body
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
 
   if (!body.name || !body.number) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
+    return response.status(400).json({
+      error: "content missing",
+    });
   }
 
-  if (persons.map(p => p.name).includes(body.name)) {
-    return response.status(400).json({ 
-      error: 'name must be unique' 
-    })
+  if (persons.map((p) => p.name).includes(body.name)) {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
   }
 
-  if (persons.map(p => p.number).includes(body.number)) {
-    return response.status(400).json({ 
-      error: 'number must be unique' 
-    })
+  if (persons.map((p) => p.number).includes(body.number)) {
+    return response.status(400).json({
+      error: "number must be unique",
+    });
   }
 
   const person = {
     name: body.name,
     number: body.number,
     id: generateId(),
-  }
+  };
 
-  persons = persons.concat(person)
+  persons = persons.concat(person);
 
-  response.json(person)
-})
+  response.json(person);
+});
 
 app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(p => p.id !== id)
+  const id = Number(request.params.id);
+  persons = persons.filter((p) => p.id !== id);
 
-  response.status(204).end()
-})
+  response.status(204).end();
+});
 
 const PORT = 3001;
 app.listen(PORT, () => {
