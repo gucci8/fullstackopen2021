a583744cf4f802e666720c62f24691378e2a4a1b
gucci8 <kusti.hamalainen@aalto.fi>
2021-05-21 18:26:47 +0300

    3.19-3.21 submission

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,56 +1,63 @@
-import React, { useState, useEffect } from 'react'
-import Filter from './components/Filter.js'
-import Persons from './components/Persons.js'
-import PersonForm from './components/PersonForm.js'
-import Notification from './components/Notification.js'
-import service from './services/util.js'
-import './index.css'
+import React, { useState, useEffect } from "react";
+import Filter from "./components/Filter.js";
+import Persons from "./components/Persons.js";
+import PersonForm from "./components/PersonForm.js";
+import Notification from "./components/Notification.js";
+import service from "./services/util.js";
+import "./index.css";
 
 const App = () => {
-  const [ persons, setPersons ] = useState([]) 
-  const [ newName, setNewName ] = useState('')
-  const [ newNumber, setNewNumber ] = useState('')
-  const [ filtStr, setFiltStr ] = useState('')
-  const [ errorMessage, setErrorMessage ] = useState({msg: null, color: 'red'})
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [filtStr, setFiltStr] = useState("");
+  const [errorMessage, setErrorMessage] = useState({ msg: null, color: "red" });
 
   const showError = (message, color) => {
-    setErrorMessage({msg: message, color: color})
+    setErrorMessage({ msg: message, color: color });
     setTimeout(() => {
-      setErrorMessage({msg: null})
-    }, 3000)
-  }
+      setErrorMessage({ msg: null });
+    }, 3000);
+  };
 
   const addPerson = (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
-    if (persons.map(p => p.name).includes(newName)) {
-      if (window.confirm(`${newName} is already added to the phonebook, replace the old number with a new one?`)) {
-        const p = persons.find(o => o.name.includes(newName))
-        const changedObj = { ...p, number: newNumber }
-        service
-          .update(p.id, changedObj)
-          .then(returnedPerson => {
-            setPersons(persons.map(o => o.id !== p.id ? o : returnedPerson))
-            setNewName('')
-            setNewNumber('')
-            showError(`Changed ${returnedPerson.name}'s number`, 'green')
-          })
+    if (persons.map((p) => p.name).includes(newName)) {
+      if (
+        window.confirm(
+          `${newName} is already added to the phonebook, replace the old number with a new one?`
+        )
+      ) {
+        const p = persons.find((o) => o.name.includes(newName));
+        const changedObj = { ...p, number: newNumber };
+        service.update(p.id, changedObj).then((returnedPerson) => {
+          setPersons(persons.map((o) => (o.id !== p.id ? o : returnedPerson)));
+          setNewName("");
+          setNewNumber("");
+          showError(`Changed ${returnedPerson.name}'s number`, "green");
+        })
+        .catch((error) => {
+          showError(`${error.response.data.error}`, "red");
+        });
       }
-      return
+      return;
     }
 
-    const personObj = { name: newName, number: newNumber }
+    const personObj = { name: newName, number: newNumber };
 
     service
       .create(personObj)
-      .then(newPerson => {
-        setPersons(persons.concat(newPerson))
-        setNewName('')
-        setNewNumber('')
-        showError(`Added ${newPerson.name}`, 'green')
+      .then((newPerson) => {
+        setPersons(persons.concat(newPerson));
+        setNewName("");
+        setNewNumber("");
+        showError(`Added ${newPerson.name}`, "green");
       })
-  }
-
+      .catch((error) => {
+        showError(`${error.response.data.error}`, "red");
+      });
+  };
 
   const deletePerson = (name, id) => {
     return () => {
@@ -58,53 +65,64 @@ const App = () => {
         service
           .del(id)
           .then(() => {
-            setPersons(persons.filter(o => o.id !== id))
-            showError(`Removed ${name} successfully.`, 'green')
+            setPersons(persons.filter((o) => o.id !== id));
+            showError(`Removed ${name} successfully.`, "green");
           })
-          .catch(error => showError(`Information of ${name} has already been removed from server`, 'red'))
+          .catch((error) =>
+            showError(
+              `Information of ${name} has already been removed from server`,
+              "red"
+            )
+          );
       }
-    }
-  }
-
+    };
+  };
 
   const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
+    setNewName(event.target.value);
+  };
 
   const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
+    setNewNumber(event.target.value);
+  };
 
   const handleFiltStrChange = (event) => {
-    setFiltStr(event.target.value)
-  }
+    setFiltStr(event.target.value);
+  };
 
   useEffect(() => {
-    service
-      .getAll()
-      .then(initialPersons => {
-        setPersons(initialPersons)
-      })
-  }, [])
+    service.getAll().then((initialPersons) => {
+      setPersons(initialPersons);
+    });
+  }, []);
 
   return (
     <div>
       <h2>Phonebook</h2>
 
-      <Notification message={errorMessage.msg} color={errorMessage.color}/>
+      <Notification message={errorMessage.msg} color={errorMessage.color} />
 
       <Filter filtCond={filtStr} handler={handleFiltStrChange} />
 
       <h3>Add a new</h3>
 
-      <PersonForm addNew={addPerson} newName={newName} nameHandler={handleNameChange} newNumber={newNumber} numHandler={handleNumberChange} />
+      <PersonForm
+        addNew={addPerson}
+        newName={newName}
+        nameHandler={handleNameChange}
+        newNumber={newNumber}
+        numHandler={handleNumberChange}
+      />
 
       <h3>Numbers</h3>
 
-      <Persons persons={persons} filtStr={filtStr} deletePerson={deletePerson} />
-      
+      <Persons
+        persons={persons}
+        filtStr={filtStr}
+        deletePerson={deletePerson}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- part3/index.js
+++ part3/index.js
@@ -13,13 +13,8 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 
-const generateId = () => {
-  let id = Math.floor(Math.random() * 10000);
-  while (persons.map((p) => p.id).includes(id)) {
-    id = Math.floor(Math.random() * 10000);
-  }
-  return id;
-};
+const generateId = () => Math.floor(Math.random() * 10000);
+
 
 app.get("/api/persons", (request, response) => {
   Person.find({}).then((persons) => {
@@ -52,36 +47,38 @@ app.get("/info", (request, response) => {
 app.post("/api/persons", (request, response) => {
   const body = request.body;
 
-  if (!body.name || !body.number) {
-    return response.status(400).json({
-      error: "content missing",
-    });
-  }
-
   const person = new Person({
     name: body.name,
     number: body.number,
     id: generateId(),
   });
 
-  person.save().then((person) => {
-    response.json(person);
-  });
+  person.save()
+    .then(savedPerson => {
+      return savedPerson.toJSON();
+    })
+    .then(formattedPerson => {
+      response.json(formattedPerson);
+    })
+    .catch(error => {
+      return response.status(400).json({
+        error: error.message,
+      });
+    });
 });
 
 app.put("/api/persons/:id", (request, response) => {
   const body = request.body;
 
-  const person = {
-    name: body.name,
-    number: body.number,
-  };
-
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(request.params.id, { number: body.number }, { new: true, runValidators: true })
     .then((result) => {
       response.json(result);
     })
-    .catch((error) => next(error));
+    .catch((error) => {
+      return response.status(400).json({
+        error: error.message,
+      });
+    });
 });
 
 app.delete("/api/persons/:id", (request, response) => {
@@ -89,8 +86,13 @@ app.delete("/api/persons/:id", (request, response) => {
     .then((result) => {
       response.status(204).end();
     })
-    .catch((error) => next(error));
+    .catch((error) => {
+      return response.status(400).json({
+        error: error.message,
+      });
+    });
 });
+
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
@@ -102,7 +104,11 @@ const errorHandler = (error, request, response, next) => {
   console.error(error.message);
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
-  }
+  } else if (error.name === "MongoError") {
+    return response.status(400).json({
+      error: error.message
+    });
+  };
   next(error);
 };
 
